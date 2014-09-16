e62b40c4743daf585e4897a902f6a7653a9322b7
ahan1874 <ahan1874@outlook.com>
2014-09-16 14:22:10 -0700

    fix bugs on H2O

--- Coding/Coding/H2O.cs
+++ Coding/Coding/H2O.cs
@@ -5,8 +5,8 @@ namespace Coding
 {
     class H2O
     {
-        private static Semaphore _H = new Semaphore(0, 10);
-        private static Semaphore _O = new Semaphore(0, 10);
+        private static Semaphore _H = new Semaphore(0, 20);
+        private static Semaphore _O = new Semaphore(0, 20);
 
         private static int _hCount = 0;
         private static int _oCount = 0;
@@ -19,42 +19,51 @@ namespace Coding
 
         public static void H(object num)
         {
-            if (_hCount > 1 && _oCount > 0)
-            {
-                _hCount -= 2;
-                _oCount -= 1;
-                _H.Release(2);
-                _O.Release();
-                Console.WriteLine("H2O made!");
-            }
+            //Thread.Sleep(10);
+            //if (_hCount > 1 && _oCount > 0)
+            //{
+            //    _hCount -= 2;
+            //    _oCount -= 1;
+            //    _H.Release(2);
+            //    _O.Release();
+            //    Console.WriteLine("H H2O made!");
+            //}
 
-            _hCount ++;
-            _H.WaitOne();
+            //_hCount ++;
+            //_O.WaitOne();
+            //Console.WriteLine("H H2O made!");
+            _H.Release();
         }
 
         public static void O(object num)
         {
-            if (_hCount > 1 && _oCount > 0)
-            {
-                _hCount -= 2;
-                _oCount -= 1;
-                _H.Release(2);
-                _O.Release();
-                Console.WriteLine("H2O made!");
-            }
+            //if (_hCount > 1 && _oCount > 0)
+            //{
+            //    _hCount -= 2;
+            //    _oCount -= 1;
+            //    _H.Release(2);
+            //    _O.Release();
+            //    Console.WriteLine("O H2O made!");
+            //}
 
-            _oCount++;
-            _O.WaitOne();
+            //_oCount++;
+            _H.WaitOne();
+            _H.WaitOne();
+            Console.WriteLine("O H2O made!");
+            _O.Release();
         }
 
 
         public static void Simulate()
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 20; i++)
             {
                 Thread hWorker = new Thread(new ParameterizedThreadStart(H2O.H));
-                Thread oWorker = new Thread(new ParameterizedThreadStart(H2O.O));
                 hWorker.Start();
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Thread oWorker = new Thread(new ParameterizedThreadStart(H2O.O));
                 oWorker.Start();
             }
         }
