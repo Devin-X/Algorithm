c7bac75f0271c55116151699e4384df6384cb57c
ahan1874 <ahan1874@outlook.com>
2014-09-27 18:59:10 -0700

    Subset II

--- Coding/Coding/SubSet.cs
+++ Coding/Coding/SubSet.cs
@@ -9,7 +9,6 @@ namespace Coding
     class SubSet
     {
         private static int combinationStartIndex = 0;
-
         public static List<List<int>> ret = new List<List<int>>();
 
         public static void FullCombination(int[] str)
@@ -69,6 +68,7 @@ namespace Coding
 
         }
 
+        public static List<List<int>> previous = new List<List<int>>();
         public static void FullCombinationRecursiveWithDup(int[] str)
         {
             if (ret.Count == 0)
@@ -81,23 +81,25 @@ namespace Coding
 
             if (combinationStartIndex > 0 && str[combinationStartIndex] == str[combinationStartIndex - 1])
             {
-                List<int> temp = new List<int>(ret.Last<List<int>>());
-                temp.Add(str[combinationStartIndex]);
-                ret.Add(temp);
+                foreach (List<int> temp in previous)
+                {
+                    temp.Add(str[combinationStartIndex]);
+                    ret.Add(new List<int>(temp));
+                }
             }
             else
             {
-                List<List<int>> tempList = new List<List<int>>();
+                previous.Clear();
                 foreach (List<int> set in ret)
                 {
                     List<int> temp = new List<int>(set.ToArray());
                     temp.Add(str[combinationStartIndex]);
-                    tempList.Add(temp);
+                    previous.Add(temp);
                 }
 
-                foreach (List<int> set in tempList)
+                foreach (List<int> set in previous)
                 {
-                    ret.Add(set);
+                    ret.Add(new List<int>(set));
                 }
             }
 
@@ -112,7 +114,30 @@ namespace Coding
             //FullCombination(a);
             //FullCombinationRecursive(a);
             FullCombinationRecursiveWithDup(a);
+            foreach (List<int> l in ret)
+            {
+                foreach (int t in l)
+                    Console.Write(t);
+                Console.WriteLine();
+            }
 
+            combinationStartIndex = 0;
+            previous.Clear();
+            ret.Clear();
+            int[] b = { 1 };
+            FullCombinationRecursiveWithDup(b);
+            foreach (List<int> l in ret)
+            {
+                foreach (int t in l)
+                    Console.Write(t);
+                Console.WriteLine();
+            }
+
+            combinationStartIndex = 0;
+            previous.Clear();
+            ret.Clear();
+            int[] c = {1, 1, 1, 2, 2, 2};
+            FullCombinationRecursiveWithDup(c);
             foreach (List<int> l in ret)
             {
                 foreach (int t in l)
