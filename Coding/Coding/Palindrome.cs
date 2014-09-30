e55984865299bfdf217921040de329f2c86b0cd5
ahan1874 <ahan1874@outlook.com>
2014-09-30 11:39:37 -0700

    GetLeastNumof Palindrome

--- Coding/Coding/Palindrome.cs
+++ Coding/Coding/Palindrome.cs
@@ -35,7 +35,7 @@ namespace Coding
                 }
                 else
                 {
-                    if(!IsLetter(sTemp[head]))
+                    if (!IsLetter(sTemp[head]))
                     {
                         head++;
                     }
@@ -48,6 +48,78 @@ namespace Coding
             }
 
             return true;
+        }
+
+        private static int _maxPalindrome = 1;
+
+        public static int GetLeastNumToConvert(string s)
+        {
+            if (s.Length == 0 || s.Length == 1)
+                return 0;
+
+            int[] leastNum = new int[s.Length];
+
+            for (int j = s.Length; j > 0; j--) 
+            {
+                for (int i = 0; i <= s.Length - j; i++)
+                {
+                    if (IsPalindrom(s.Substring(i, j)))
+                    {
+                        _maxPalindrome = j;
+                        Console.WriteLine(MakePalindrome(s, i, j));
+                        return (s.Length - _maxPalindrome);
+                    }
+                }
+            }
+
+            return (s.Length - _maxPalindrome);
+        }
+
+
+        public static string MakePalindrome(string s, int i, int j)
+        {
+            StringBuilder sb = new StringBuilder();
+            int start = 0;
+            int end = s.Length - 1;
+
+            if (i == 0)
+            {
+                while(end > i + j-1)
+                {
+                    sb.Append(s[end--]);
+                }
+
+                sb.Append(s);
+                return sb.ToString();
+            }
+
+            if(end == i + j-1)
+            {
+                sb.Append(s);
+                while(start < i)
+                {
+                    sb.Append(s[start++]);
+                }
+                
+                return sb.ToString();
+            }
+
+            while (start < i && end > j - 1)
+            {
+                if (s[start] != s[end])
+                {
+                    sb.Append(s[end]);
+                    end--;
+                }
+                else
+                {
+                    sb.Append(s[end]);
+                    start++;
+                    end--;
+                }
+            }
+
+            return sb.Append(s.Substring(i, j)).ToString();
         }
 
         private static bool IsLetter(char c)
@@ -68,6 +140,26 @@ namespace Coding
             }
 
             return false;
+        }
+
+
+        public static void Test()
+        {
+            string s = "ab";
+            //Console.WriteLine(s);
+            //Console.WriteLine(GetLeastNumToConvert(s));
+
+            s = "abbbb";
+            Console.WriteLine(s); 
+            Console.WriteLine(GetLeastNumToConvert(s));
+
+            s = "a";
+            Console.WriteLine(s);
+            Console.WriteLine(GetLeastNumToConvert(s));
+
+            s = "abacadaeaf";
+            Console.WriteLine(s);
+            Console.WriteLine(GetLeastNumToConvert(s));
         }
     }
 }
--- Coding/Coding/Program.cs
+++ Coding/Coding/Program.cs
@@ -30,6 +30,8 @@ namespace Coding
 
             MaxNumOfCars.TestMaxNumOfCars();
             Sorting.Test();
+
+            Palindrome.Test();
         }
     }
 }
