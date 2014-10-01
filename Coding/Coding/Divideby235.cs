370b46f840d176e1df36cd0ae3a6336bdabce243
ahan1874 <ahan1874@outlook.com>
2014-09-30 17:27:04 -0700

    Divide the number by 2, 3, 5

---
+++ Coding/Coding/Divideby235.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding
+{
+    /// <summary>
+    /// An array of size n is given. The array contains digits from 0 to 9. I had to generate the maximum number using the digits in the array such that it is divisible by 2, 3 and 5 
+    /// eg: 1 array = 18760, output must be: 8160 
+    /// eg: 2 array = 7776, output must be: “no number can be formed”
+    /// </summary>
+    class Divideby235
+    {
+        //public static int GetMaxNumDivideby235(int[] array)
+        //{
+        //    List<int> list = new List<int>(array);
+        //    list.Sort();
+        //    int sum = 0;
+        //    foreach(int a in list)
+        //    {
+        //        sum += a;
+        //    }
+
+        //    if (list[0] != 0)
+        //        return -1;
+
+
+
+        //}
+    }
+}
