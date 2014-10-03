cc325ed84d526105b6a7dfd8ef7097436f606219
ahan1874 <ahan1874@outlook.com>
2014-10-03 16:38:14 -0700

    Update for the words ladder length
    This code is still buggy

--- Coding/Coding/WordsLadderLength.cs
+++ Coding/Coding/WordsLadderLength.cs
@@ -15,14 +15,12 @@ namespace Coding
         public static int index = 0;
         public static List<List<string>> FindWordLadder(string start, string end, HashSet<string> _dictionary)
         {
+            _treeStack.Add(start);
             while (_treeStack.Count > 0)
             {
                 string s = _treeStack[0];
                 _treeStack.Remove(s);
                 _dictionary.Remove(s);
-                //FindWordLadder(s, end);
-                //_dictionary.Add(s);
-                //_dictionary.Add(start);
 
                 for (int i = 0; i < s.Length; i++)
                 {
@@ -67,18 +65,18 @@ namespace Coding
 
         public static void TestGetLadderLength()
         {
-            _treeStack.Add("abc");
+            //_treeStack.Add("abc");
             //_treeStack.Add("||");
-            _levelList.Add(new List<string>());
-            HashSet<string> _dictionary = new HashSet<string>();
-            _dictionary.Add("aaa");
-            _dictionary.Add("bbc");
-            _dictionary.Add("cbc");
-            _dictionary.Add("bac");
-            _dictionary.Add("cbd");
-            _dictionary.Add("cog");
-            _dictionary.Add("hog");
-            Console.WriteLine(FindWordLadder("abc", "cbd", _dictionary));
+            //_levelList.Add(new List<string>());
+            HashSet<string> _dictionary = InitData(); // new HashSet<string>();
+            //_dictionary.Add("aaa");
+            //_dictionary.Add("bbc");
+            //_dictionary.Add("cbc");
+            //_dictionary.Add("bac");
+            //_dictionary.Add("cbd");
+            //_dictionary.Add("cog");
+            //_dictionary.Add("hog");
+            Console.WriteLine(FindWordLadder("gape", "mild", _dictionary));
 
             foreach (List<string> ls in _finalList)
             {
@@ -91,6 +89,48 @@ namespace Coding
             }
         }
 
+
+        public static HashSet<string> InitData()
+        {
+            
+            string[] data = {"dose", "ends", "dine", "jars", "prow", "soap", "guns", "hops", "cray", "hove",
+                "ella", "hour", "lens", "jive", "wiry", "earl", "mara", "part", "flue", "putt",
+               "rory", "bull", "york", "ruts", "lily", "vamp", "bask", "peer", "boat", "dens",
+                "lyre", "jets", "wide", "rile", "boos", "down", "path", "onyx", "mows", "toke",
+                "soto", "dork", "nape", "mans", "loin", "jots", "male", "sits", "minn", "sale",
+                "pets", "hugo", "woke", "suds", "rugs", "vole", "warp", "mite", "pews", "lips",
+                "pals", "nigh", "sulk", "vice", "clod", "iowa", "gibe", "shad", "carl", "huns",
+                "coot", "sera", "mils", "rose", "orly", "ford", "void", "time", "eloy", "risk",
+                "veep", "reps", "dolt", "hens", "tray", "melt", "rung", "rich", "saga", "lust",
+                "yews", "rode", "many", "cods", "rape", "last", "tile", "nosy", "take", "nope",
+                "toni", "bank", "jock", "jody", "diss", "nips", "bake", "lima", "wore", "kins",
+                "cult", "hart", "wuss", "tale", "sing", "lake", "bogy", "wigs", "kari", "magi",
+                "bass", "pent", "tost", "fops", "bags", "duns", "will", "tart", "drug", "gale",
+                "mold", "disk", "spay", "hows", "naps", "puss", "gina", "kara", "zorn", "boll",
+                "cams", "boas", "rave", "sets", "lego", "hays", "judy", "chap", "live", "bahs",
+                "ohio", "nibs", "cuts", "pups", "data", "kate", "rump", "hews", "mary", "stow",
+                "fang", "bolt", "rues", "mesh", "mice", "rise", "rant", "dune", "jell", "laws",
+                "jove", "bode", "sung", "nils", "vila", "mode", "hued", "cell", "fies", "swat",
+                "wags", "nate", "wist", "honk", "goth", "told", "oise", "wail", "tels", "sore",
+                "hunk", "mate", "luke", "tore", "bond", "bast", "vows", "ripe", "fond", "benz",
+                "firs", "zeds", "wary", "baas", "wins", "pair", "tags", "cost", "woes", "buns",
+                "lend", "bops", "code", "eddy", "siva", "oops", "toed", "bale", "hutu", "jolt",
+                "rife", "darn", "tape", "bold", "cope", "cake", "wisp", "vats", "wave", "hems",
+                "bill", "cord", "pert", "type", "kroc", "ucla", "albs", "yoko", "silt", "pock",
+                "drub", "puny", "fads", "mull", "pray", "mole", "talc", "east", "slay", "jamb",
+                "mill", "dung", "jack", "lynx", "nome", "leos", "lade", "sana", "tike", "cali",
+                "toge", "pled", "mile", "mass", "leon", "sloe", "lube", "kans", "cory", "burs",
+                "race", "toss", "mild", "tops", "maze", "city", "sadr", "bays", "poet", "volt",
+                "laze", "gold", "zuni", "shea", "gags", "fist", "ping", "pope", "cora", "yaks",
+                "cosy", "foci", "plan", "colo", "hume", "yowl", "craw", "pied", "toga", "lobs",
+                "love", "lode", "duds", "bled", "juts", "gabs", "fink", "rock", "pant", "wipe",
+                "pele", "suez", "nina", "ring", "okra", "warm", "lyle", "gape", "bead", "lead",
+                "jane", "oink", "ware", "zibo", "inns", "mope", "hang", "made", "fobs", "gamy",
+                "fort", "peak", "gill", "dino", "dina", "tier"};
+
+            HashSet<string> _dictionary = new HashSet<string>(data);
+            return _dictionary;
+        }
     }
 
 
