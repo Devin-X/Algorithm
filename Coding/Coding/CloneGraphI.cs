6f286d5961a98578906f834553141e23a233bf72
ahan1874 <ahan1874@outlook.com>
2014-10-11 16:33:39 -0700

    CloneGraph I v2 using vertice object as the reference

--- Coding/Coding/CloneGraphI.cs
+++ Coding/Coding/CloneGraphI.cs
@@ -20,6 +20,11 @@ namespace Coding
 
     class CloneGraphI
     {
+        /// <summary>
+        /// Using the data in the vertice to be the key in this version
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
         public static Vertice CloneGraph(Vertice node)
         {
             Queue<Vertice> q = new Queue<Vertice>();
@@ -58,6 +63,49 @@ namespace Coding
             return ret;
         }
 
+        /// <summary>
+        /// This version, I use the vertice as the key in the dictionary. Not the data of the vertice. 
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public static Vertice CloneGraphV2(Vertice node)
+        {
+            Queue<Vertice> q = new Queue<Vertice>();
+            q.Enqueue(node);
+            Vertice ret = new Vertice(node.data);
+            Queue<Vertice> qShadow = new Queue<Vertice>();
+            qShadow.Enqueue(ret);
+            Dictionary<Vertice, Vertice> map = new Dictionary<Vertice, Vertice>();
+            map.Add(node, ret);
+            while(q.Any())
+            {
+                Vertice current = q.Dequeue();
+                Vertice shadow = qShadow.Dequeue();
+                foreach(Vertice v in current.neighbors)
+                {
+                    if(!map.ContainsKey(v))
+                    {
+                        Vertice clone = new Vertice(v.data);
+                        shadow.neighbors.Add(clone);
+                        q.Enqueue(v);
+                        qShadow.Enqueue(clone);
+                        map.Add(v, clone);
+                    }
+                    else
+                    {
+                        Vertice clone;
+                        if(map.TryGetValue(v, out clone))
+                        {
+                            shadow.neighbors.Add(clone);
+                        }
+                        
+                    }
+                }
+            }
+
+            return ret;
+        }
+
         public static void Test()
         {
             Vertice node1 = new Vertice(1);
@@ -72,7 +120,8 @@ namespace Coding
             node4.neighbors.Add(node5);
             node4.neighbors.Add(node1);
 
-            Vertice ret = CloneGraph(node1);
+            //Vertice ret = CloneGraph(node1);
+            Vertice ret = CloneGraphV2(node1);
 
             Queue<Vertice> q = new Queue<Vertice>();
             q.Enqueue(ret);
