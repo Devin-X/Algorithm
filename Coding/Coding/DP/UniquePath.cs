1d3ea3417226514af0de903e71faf88a7e8afce1
ahan1874 <ahan1874@outlook.com>
2015-02-16 00:59:46 -0800

    UniquePathII
    Not a diffcult solution

--- Coding/Coding/DP/UniquePath.cs
+++ Coding/Coding/DP/UniquePath.cs
@@ -14,9 +14,9 @@ namespace Coding
     /// </summary>
     public class UniquePath
     {
-        public static int uniquePaths(int m, int n)
+        public static uint uniquePaths(int m, int n)
         {
-            int[,] matrix = new int[m,n];
+            uint[,] matrix = new uint[m, n];
             for (int i = 0; i < m; i++)
             {
                 for (int j = 0; j < n; j++)
@@ -30,12 +30,12 @@ namespace Coding
             {
                 for (int j = 0; j < n; j++)
                 {
-                    if( i-1 >= 0)
+                    if (i - 1 >= 0)
                     {
                         matrix[i, j] += matrix[i - 1, j];
                     }
 
-                    if( j-1 >= 0)
+                    if (j - 1 >= 0)
                     {
                         matrix[i, j] += matrix[i, j - 1];
                     }
@@ -44,6 +44,55 @@ namespace Coding
 
             return matrix[m - 1, n - 1];
 
+        }
+
+        public static int uniquePathsII(int[,] matrix, int m, int n)
+        {
+            if (matrix[0, 0] == 1)
+                return 0;
+
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                    Console.Write(" ");
+                    if (matrix[i, j] == 1)
+                    {
+                        matrix[i, j] = int.MinValue;
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            matrix[0, 0] = 1;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if(matrix[i,j] == int.MinValue)
+                        continue;
+                    if (i - 1 >= 0)
+                    {
+                        if (matrix[i - 1, j] != int.MinValue)
+                        {
+                            matrix[i, j] += matrix[i - 1, j];
+                        }
+                    }
+
+                    if (j - 1 >= 0)
+                    {
+                        if (matrix[i, j - 1] != int.MinValue)
+                        {
+                            matrix[i, j] += matrix[i, j - 1];
+                        }
+                    }
+                }
+            }
+
+            return matrix[m - 1, n - 1];
         }
 
         public static void Test()
@@ -55,6 +104,24 @@ namespace Coding
             Console.WriteLine(string.Format("{0} * {1}  --> {2}", 3, 3, uniquePaths(3, 3)));
 
             Console.WriteLine(string.Format("{0} * {1}  --> {2}", 4, 5, uniquePaths(4, 5)));
+            Console.WriteLine(string.Format("{0} * {1}  --> {2}", 100, 100, uniquePaths(100, 100)));
+
+            int[,] matrix = new int[3, 3] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
+            
+            Console.WriteLine(string.Format("{0} * {1}  --> {2}", 3, 3, uniquePathsII(matrix, 3, 3)));
+
+            matrix = new int[3, 3] { { 0, 0, 1 }, { 0, 1, 0 }, { 1, 0, 0 } };
+
+            Console.WriteLine(string.Format("{0} * {1}  --> {2}", 3, 3, uniquePathsII(matrix, 3, 3)));
+
+            matrix = new int[3, 3] { { 0, 0, 1 }, { 0, 1, 0 }, { 0, 0, 0 } };
+
+            Console.WriteLine(string.Format("{0} * {1}  --> {2}", 3, 3, uniquePathsII(matrix, 3, 3)));
+
+
+            matrix = new int[3, 4] { { 0, 0, 0, 0 }, { 0, 1, 0, 0}, { 0, 0, 0, 0} };
+
+            Console.WriteLine(string.Format("{0} * {1}  --> {2}", 3, 4, uniquePathsII(matrix, 3, 4)));
         }
     }
 }
