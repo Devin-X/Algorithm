1d327573cb1c70e3cc2813c2d2a94f2b0f798e87
ahan1874 <ahan1874@outlook.com>
2015-07-20 09:43:26 -0700

    Count digit one, not yet finished

---
+++ Coding/Coding/CountDigitOne.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding
+{
+    public class CountDigitOneSolution
+    {
+        public static int CountDigitOne(int n)
+        {
+            if (n <= 0)
+                return 0;
+            int cache = 1;
+            for (int i = 1; i < n; i++)
+            {
+                if (i > 1 && i < 9)
+                    i = 9;
+                if (i > 20 && i < 100)
+                    
+
+                int t = Count(i + 1);
+                if (t > 0)
+                    cache += t;
+            }
+
+            return cache;
+        }
+
+        public static int Count(int n)
+        {
+            int ret = 0;
+            while (n > 0)
+            {
+                if (n%10 == 1)
+                    ret++;
+                n = n/10;
+            }
+
+            return ret;
+        }
+    }
+}
