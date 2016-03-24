9b251db40fddd5b6af75c1b49e94d88617682bc2
ahan1874 <ahan1874@outlook.com>
2016-03-23 17:49:57 -0700

    increasing triplet subsequence

--- Coding/Coding/CodesBackup.cs
+++ Coding/Coding/CodesBackup.cs
@@ -2317,3 +2317,59 @@ public class Solution
         return cache[start, end];
     }
 }
+
+
+Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array. 
+
+Formally the function should:
+
+
+Return true if there exists i, j, k  
+ such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false. 
+
+
+Your algorithm should run in O(n) time complexity and O(1) space complexity. 
+
+Examples:
+ Given [1, 2, 3, 4, 5],
+ return true. 
+
+Given [5, 4, 3, 2, 1],
+ return false. 
+
+
+public class Solution {
+    public bool IncreasingTriplet(int[] nums) {
+        if(nums.Length<3) return false;
+        
+        //int left = 0;
+        //int right = nums.Length-1;
+        //bool[] biggerLeft = new bool[nums.Length];
+        
+        //for(int i = 1; i < nums.Length; i++){
+        //    if(nums[i] < nums[left])  left = i;
+        //    if(nums[i]>nums[left]) biggerLeft[i] = true;
+        //}
+        
+        //for(int i = nums.Length-2; i > 0; i--){
+        //    if(nums[i] > nums[right]){
+        //      right = i;
+        //    }
+            
+        //    if(nums[i] < nums[right] && biggerLeft[i]) return true;
+        //}
+        
+        
+        int min = nums[0]; int mid = int.MaxValue;
+        for(int i = 1; i < nums.Length; i++){
+            if(nums[i] < min) min = nums[i];
+            
+            if(nums[i] > min && nums[i] < mid){
+                mid = nums[i];
+            }
+            
+            if(nums[i] > mid) return true;
+        }
+        return false;
+    }
+}
