b7523db2c6f4e6141ccd6b627c008792e3ad9e90
ahan1874 <ahan1874@outlook.com>
2016-03-28 16:27:25 -0700

    H-index

--- Coding/Coding/CodesBackup.cs
+++ Coding/Coding/CodesBackup.cs
@@ -2319,8 +2319,6 @@ public class Solution
 }
 
 
-<<<<<<< HEAD
-
 Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'.
 
 A region is captured by flipping all 'O's into 'X's in that surrounded region.
@@ -2642,6 +2640,36 @@ public class Solution {
         }else{
             QuickAdjust(kL, middle+1, end, target);
         }
->>>>>>> b191d2b19050e06db18300f561003ff4e0614629
+    }
+}
+
+
+Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index. 
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each." 
+
+For example, given citations = [3, 0, 6, 1, 5], which means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively. Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, his h-index is 3. 
+
+Note: If there are several possible values for h, the maximum one is taken as the h-index. 
+
+public class Solution {
+    public int HIndex(int[] citations) {
+        int n = citations.Length; 
+        return bs(citations, 0, n-1);
+    }
+    
+    private int bs(int[] c, int start, int end){
+        if(start> end) return 0;
+        int middle = start + (end-start)/2;
+        
+        int h = c.Length-middle;
+        int max = 0;
+        if(h <= c[middle]){
+            max = Math.Max(bs(c, start, middle-1), h);
+        }else{
+            max = bs(c, middle+1, end);
+        }
+        
+        return max;
     }
 }
