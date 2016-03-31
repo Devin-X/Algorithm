b4c32fc62aba8d4e631726d233460fe625a569a6
ahan1874 <ahan1874@outlook.com>
2016-03-31 10:09:35 -0700

    TestPath Sum
    #	Coding/Coding/CodesBackup.cs

--- Coding/Coding/Program.cs
+++ Coding/Coding/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.Net;
+using System.Runtime.InteropServices;
 using Coding.Array;
 using Coding.Tree;
 
@@ -12,14 +13,106 @@ namespace Coding
 {
     class Program
     {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+
+        public class TreeNoden
+        {
+            public int val;
+            public TreeNoden left;
+            public TreeNoden right;
+
+            public TreeNoden(int x)
+            {
+                val = x;
+            }
+        }
+        public static IList<TreeNode> GenerateTrees(int n) {
+        List<TreeNode>[,] cache = new List<TreeNode>[n+1,n+1];
         
+        for(int i = 0; i <= n; i++){
+            cache[i,i]= new List<TreeNode>();
+            cache[i,i].Add(new TreeNode(i));
+        }
+        if(n == 0) return cache[0,0]; 
+        
+        for(int i = 1; i <= n ; i++){
+            for(int j = 1; j <= i; j++){
+                
+                List<TreeNode> left;
+                List<TreeNode> right;
+                if(j-1 >=1) left = cache[1,j-1];
+                else {left = new List<TreeNode>(); left.Add(null);}
+                if(j+1 <= i) right = cache[j+1, i];
+                else {right = new List<TreeNode>(); right.Add(null);}                
+                
+                foreach (TreeNode ln in left){
+                    foreach (TreeNode rn in right){
+                        TreeNode root = new TreeNode(j);
+                        root.left = ln;
+                        root.right = rn;
+                        
+                        if(cache[1, j] == null) cache[1,j] = new List<TreeNode>();
+                        
+                        cache[1,j].Add(root);
+                    }
+                }
+            }
+        }
+        
+        return cache[1,n];
+    }
+        private static void bt(TreeNoden root, int sum, int s, List<int> current, IList<IList<int>> final)
+        {
+        current.Add(root.val);
+        s += root.val;
+        
+        if(root.left == null && root.right == null){
+            if(sum == s){
+                final.Add(new List<int>(current));
+            }
+        }
+        
+        if(root.left != null) bt(root.left, sum, s, current, final);
+        if(root.right != null) bt(root.right, sum, s, current, final);
+        
+        current.Remove(current.Count-1);
+        s -= root.val;
+    }
         static void Main(string[] args)
         {
             MaxSlidingWindowSolution.Test();
             MinWindowString.Test();
             LCABST.Test();
+            
+             IList<IList<int>> final = new List<IList<int>>(1000);
+             List<int> current = new List<int>();
+            int s = 0;
 
-            int[] height = { 1, 1 };
+            TreeNoden root = new TreeNoden(0);
+            TreeNoden r = root;
+            for (int i = 0; i < 5000; i++)
+            {
+                TreeNoden newNode = new TreeNoden(1);
+                root.left = newNode;
+                root.right = null;
+                root = root.left;
+            }
+            
+
+        
+             bt(r, 5000, s, current, final);
+
+            Console.Write(final.Count);
 
             //MinStack ms = new MinStack();
             //ms.Push(-2);
